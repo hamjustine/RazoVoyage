00411164d2070d805613dfe0a676a0a4d47e5f56
vagrant <vagrant@homestead>
2019-05-20 12:25:28 +0000

    correction route folder

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return " Ceci est la page d’accueil";
+});
+
+Route::get('/voyages', function () {
+    return " Ceci est la page qui affichera tous les voyages";
+});
+
+Route::get('/voyages/id', function () {
+    return " Ceci est la page qui affichera le detail
+    d’un voyage identifié par $id_voyage";
+});
+
+Route::get('/a_propos', function () {
+    return " Ceci est la page qui affichera les
+    informations concernant l’entreprise";
+});
+
+Route::get('/admin/voyage', function () {
+    return " Ceci est la page qui affichera les
+    informations concernant l’entreprise";
+});
+
+Route::get('/admin/users', function () {
+    return "Ceci est la page users de la console
+    d’administration";
 });
